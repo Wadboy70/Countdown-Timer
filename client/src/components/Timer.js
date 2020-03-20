3e3e668723a0a273fa43979518a594098a29f90a
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-03-19 22:48:41 -0400

    figured out counting

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Timer.css";
 
 const getTime = (props) => {
+    const targetDate = {};
     var rawDate = props.counterInfo.date;
-    var year = rawDate.slice(0,4);
-    var month = rawDate.slice(5,7);
-    var day = rawDate.slice(8);
-    return year +" "+ month + " " + day;
+    targetDate.year = rawDate.slice(0,4);
+    targetDate.month = rawDate.slice(5,7);
+    targetDate.day = rawDate.slice(8);
+
 
 }
 const Timer = (props) => {
+    const [currTime, setCurrTime] = useState(undefined);
+
+    useEffect (() => {
+        setInterval(() => setCurrTime(new Date().getSeconds()), 1000);
+        document.title = currTime;
+        return(clearInterval());
+    });
     return(
         <>
             {props.counterInfo && <div>
                 <h2>{props.counterInfo.name}</h2>
-                <p>{getTime(props)}</p>
+                
             </div>}
+            <p>{currTime}</p>
         </>
     );
 }
