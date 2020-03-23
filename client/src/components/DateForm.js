c0736100c72bebb8b4866d13bf8657c5f7d031e0
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-03-23 13:34:31 -0400

    edited dateForm

--- client/src/components/DateForm.js
+++ client/src/components/DateForm.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './DateForm.css';
 
 var refresh = (showForm) =>{
     return showForm ? "reset" : "";
 }
+
 var getTodayDate = () => {
     var today = new Date();
     var minDay = String(today.getDate()).padStart(2, '0');
@@ -11,17 +12,49 @@ var getTodayDate = () => {
     var minYear = today.getFullYear();
     return (minYear + "-" + minMonth + "-" + minDay);
 }
+
 const submitDate = (props, dateInfo) =>{
     props.setCounterInfo(dateInfo); //passes date info to be used in countdown
     props.setShowInputForm(false);
     refresh(props.showForm);
     console.log(dateInfo);
 }
+
+const warningMessage = (dateInfo) => {
+}
 const DateForm = (props) => {
+    //will be submitted with form
     const dateInfo = {
         name: "",
-        date: ""
+        date: {
+            year: 2020,
+            month: 1,
+            day: 1
+        }
     }
+
+    //Year Array
+    var arr = [];
+    for(var i = 0; i < 50; i++){
+        arr.push(new Date().getFullYear() + i);
+    }
+    const years = arr.map(year =>  year = <option>{year}</option>);
+
+    //month Array
+    const months =['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'].map((month,index) => {
+        return <option value = {index+1}> {month} </option>;
+    });
+
+    arr = [];
+    for (var i = 0; i < 31; i++){
+        arr.push(i+1);
+    }
+
+    //day Array
+    const days = arr.map(day => day = <option value = {day}>{day}</option> );
+
+    var shouldWarn = false;
+    var warningMessage = "";
     return(
         <>
             <h2>new countdown</h2>
@@ -32,21 +65,29 @@ const DateForm = (props) => {
                 type = "text"
                 placeholder = "Name" 
                 onChange = {e => dateInfo.name = e.target.value}/>
-                
-                <input 
-                required
-                type="date" 
-                min={getTodayDate()} 
-                onChange = {e => dateInfo.date = e.target.value}/>
+
+                <select onChange = {e => dateInfo.year = e.target.value}>
+                    {years}
+                </select>
+
+                <select onChange = {e => dateInfo.month = e.target.value}>
+                    {months}
+                </select>
+                    
+                <select onChange = {e => dateInfo.day = e.target.value}>
+                    {days}
+                </select>
 
                 <button 
                 type = "submit" 
                 onClick = {(e) => {
                         e.preventDefault();
-                        submitDate(props, dateInfo);
+                        submitDate(props, dateInfo, shouldWarn, warningMessage);
                     }
                 }>
                 Submit</button>
+
+                {shouldWarn && <p>{warningMessage(shouldWarn)}</p>}
             </form>
         </>
     );
